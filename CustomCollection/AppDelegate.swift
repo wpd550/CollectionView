40c8f50683e97a4eabd875368199ed26b1651a13
Tenorshare <Tenorshare@Tenorshare.cn>
2020-12-15 17:22:47 +0800

    Initial Commit

---
+++ CustomCollection/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CustomCollection
+//
+//  Created by dong wu on 2020/12/15.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CustomCollection/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CustomCollection
+//
+//  Created by dong wu on 2020/12/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
